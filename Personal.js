ec9cd21891dc2df5b500b60ecbc915a351343d08
TaniaRG20 <163084299+TaniaRG20@users.noreply.github.com>
2024-03-12 13:58:21 -0600

    Add files via upload

---
+++ Personal.js
@@ -0,0 +1,8 @@
+function muestraoculta(id){
+    let div =document.getElementById(id);
+    if (div.style.display == "none") {
+        div.style.display = "flex";
+    }
+    else { 
+        div.style.display = "none"; }
+}
